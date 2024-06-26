bbe7b5cc6041636acdb66af88a2cceb4e07ebe32
wolfgunblood <jalajdorai1@gmail.com>
2024-06-26 15:23:27 +0530

    system

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           <StackTheme>
             <ThemeProvider
               attribute="class"
-              defaultTheme="light"
+              defaultTheme="system"
               enableSystem
               disableTransitionOnChange
             >
