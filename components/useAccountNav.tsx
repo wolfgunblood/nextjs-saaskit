abb690f4f196609e196447bff3fe03944eb42896
wolfgunblood <jalajdorai1@gmail.com>
2024-06-26 13:35:16 +0530

    seeing avatar

--- components/useAccountNav.tsx
+++ components/useAccountNav.tsx
@@ -22,11 +22,15 @@ export function UserAccountNav() {
   const app = useStackApp();
   const user = app.useUser();
 
+  // Define a default image URL
+  const defaultImageUrl = "https://avatars.dicebear.com/api/avataaars/avatar1.svg"; // Update this path to your default image
+
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
       <img
-          src={user?.profileImageUrl}
+          src={user?.profileImageUrl || defaultImageUrl}
           alt="User Avatar"
           style={{
             width: "40px",
